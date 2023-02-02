1e7861f999e60aca52c46cf6fa754c2526a78f38
Jae Ung Jung <roosa0210@gmail.com>
2023-02-02 17:05:26 +0900

    Exercise-Chapter1-Q4-output_update

--- Exercise-Chapter1-Q4.py
+++ Exercise-Chapter1-Q4.py
@@ -2,11 +2,13 @@
 data = input('숫자로 이루어진 문자열을 입력하세요. ')
 
 result = int(data[0])
+print(data[0], end='')
 for i in range(len(data)-1):
     if(result * int(data[i+1]) >= result + int(data[i+1])):
         result *=  int(data[i+1])
+        print( " X " + data[i+1], end = '')
     else:
-        result = result + int(data[i+1])
+        result += int(data[i+1])
+        print(" + " + data[i+1], end = '')
 
-
-print(result)
+print(" = " + str(result))
